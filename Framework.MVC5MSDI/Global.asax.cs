f419a7470626b50a7866ad014ccbfeb22b180674
JiaSyuan Li <jiasyuan@hotmail.com>
2020-06-03 22:30:12 +0800

    Setting DI

--- Framework.MVC5MSDI/Global.asax.cs
+++ Framework.MVC5MSDI/Global.asax.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.UI;
+using Framework.MVC5MSDI.Resolver;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Framework.MVC5MSDI
@@ -23,11 +24,24 @@ namespace Framework.MVC5MSDI
             //建立ServiceCollection
             var services = new ServiceCollection();
             ConfigureServices(services);
+
+            //自訂Resolver
+            var resolver = new CoreDIDependencyResolver(services.BuildServiceProvider());
+            DependencyResolver.SetResolver(resolver);
         }
 
         private void ConfigureServices(ServiceCollection services)
         {
-            throw new NotImplementedException();
+            //找出所有的Controller
+            var controllers = typeof(MvcApplication).Assembly.GetExportedTypes()//所有public class
+                            .Where(t => !t.IsAbstract)//非抽象類別
+                            .Where(t => typeof(IController).IsAssignableFrom(t))//繼承IController
+                            .Where(t => t.Name.EndsWith("Controller"))//名稱以Controller結尾
+                            .ToList();
+            //將Controller加入ServiceCollection
+            controllers.ForEach(ctrl => services.AddTransient(ctrl));
+            //加入HttpClient
+            services.AddHttpClient();
         }
     }
 }
