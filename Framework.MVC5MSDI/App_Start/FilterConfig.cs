bed6b60be3d58c59f4d88ff0090b5c6f8f06edd4
JiaSyuan Li <jiasyuan@hotmail.com>
2020-06-03 21:42:54 +0800

    Create MVC 5 Project

---
+++ Framework.MVC5MSDI/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Framework.MVC5MSDI
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Framework.MVC5MSDI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Framework.MVC5MSDI.MvcApplication" Language="C#" %>
