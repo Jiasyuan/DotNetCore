f7f710efd28ea3b4aac91e61b717a78748976ab6
JiaSyuan Li <jiasyuan@hotmail.com>
2020-06-03 23:38:13 +0800

    Logging

--- Framework.MVC5MSDI/Controllers/HomeController.cs
+++ Framework.MVC5MSDI/Controllers/HomeController.cs
@@ -1,20 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web;
+﻿using System.Net.Http;
 using System.Web.Mvc;
-using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Logging;
 
 namespace Framework.MVC5MSDI.Controllers
 {
     public class HomeController : Controller
     {
         private readonly HttpClient httpClient;
+        private readonly ILogger<HomeController> logger;
 
-        public HomeController(IHttpClientFactory httpClientFactory)
+        public HomeController(IHttpClientFactory httpClientFactory, ILogger<HomeController> logger)
         {
             this.httpClient = httpClientFactory.CreateClient();
+            this.logger = logger;
         }
 
         public ActionResult Index()
@@ -25,6 +23,7 @@ namespace Framework.MVC5MSDI.Controllers
         public async System.Threading.Tasks.Task<ActionResult> About()
         {
             var result = await httpClient.GetStringAsync("https://apiservice.mol.gov.tw/OdService/download/A17030000J-000047-cEE");
+            logger.LogDebug(result);
             ViewBag.Message = result;
 
             return View();
