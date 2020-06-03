d922df22580a74a4cb25b596002f83919a91779b
JiaSyuan Li <jiasyuan@hotmail.com>
2020-06-03 22:24:21 +0800

    Create CoreDIDependencyResolver

---
+++ Framework.MVC5MSDI/Resolver/CoreDIDependencyResolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Framework.MVC5MSDI.Resolver
+{
+    public class CoreDIDependencyResolver :IDependencyResolver
+    {
+        private ServiceProvider serviceProvider;
+
+        public CoreDIDependencyResolver(ServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
